3dc702d668c02fa971dc4f386db3a75dec6aa30b
dadeo <drobertwatson@gmail.com>
2009-10-09 19:08:02 -0500

    moving out of learning project to project specifically for pdf dsl

--- src/main/scala/learning/scala/Hello.scala
+++
@@ -1,8 +0,0 @@
-package learning.scala
-
-
-
-class Hello {
-  def sweller() = println("this is real swell")
-  def greet(name:String) = printf("hello %s!", name)
-}
--- src/main/scala/learning/scala/Trials.scala
+++
@@ -1,8 +0,0 @@
-//implicit def elvisOperator[T](alt: =>T) = new {
-//  def ?:[A >: T](pred: A) = if (pred == null) alt else pred
-//}
-//
-//val result:String = null ?: "hello" ?: "there"
-//
-//println(result)
-
